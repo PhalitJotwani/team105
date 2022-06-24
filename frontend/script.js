2a2beea13807dd3b322202ae56b99ccf3cb65d0a
Meghana Ayyala Somayajula <76214336+meghanaayyala@users.noreply.github.com>
2022-06-24 14:48:33 +0530

    added scroll

--- frontend/script.js
+++ frontend/script.js
@@ -28,3 +28,12 @@ $('.counting').each(function() {
   
 
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
